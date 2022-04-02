ce698513bfeb02635d823b09a7d2be2c00efd91b
Kim <kmdnpm@gmail.com>
2022-04-02 03:01:37 -0400

    added an else statement

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 let userName = window.prompt('Please enter your name:')
 
 for(let i=0; i<questions.length; i++){
-    console.log(questions[i])
+    let question = questions[i];
+    let userAnswer = window.prompt(questions[i].text)
+    if(userAnswer === question.correctAnswer){
+        console.log('Correct')
+    }else{
+        console.log('Incorrect')
+    }
 }
